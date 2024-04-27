167fe19b6e8a71b433b718d6539cfaab99f5d66f
Chenter <chenterphai61@gmil.com>
2024-04-27 16:31:34 +0700

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
+const photoHolder = require('./assets/background-image.png')
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ color: '#fff' }}>Open up App.js to start working</Text>
+      <View style={styles.imageContainer}>
+        <Image
+          source={photoHolder}
+          style={styles.image}
+        />
+      </View>
+
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -13,8 +21,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18
+  }
+
 });
